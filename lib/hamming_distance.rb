aa4d200eb923d74bcda0fcb98e2586d4581710b6
fishermanswharff <fishermanswharff@github.com>
2015-01-15 09:51:49 -0500

    solution

--- lib/hamming_distance.rb
+++ lib/hamming_distance.rb
@@ -1,5 +1,15 @@
+require 'byebug'
+
 class StringsUnequalLength < ArgumentError
 end
 
 def hamming_distance(string_1, string_2)
+  hamming_distance = 0
+  if string_1.length == string_2.length
+    arr1, arr2 = string_1.split(""), string_2.split("")
+    arr1.each_with_index { |item,index| item == arr2[index] ? nil : hamming_distance += 1 }
+  else
+    raise StringsUnequalLength
+  end
+  hamming_distance
 end
